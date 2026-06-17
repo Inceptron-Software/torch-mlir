2342102cdac60abe999d9dc914c83cacf579ba7a
Alexandru Dura <alexandru.dura@inceptron.io>
2026-06-17 21:31:37 +0000

    Fix decomposition of aten.nonzero

--- lib/Dialect/Torch/Transforms/DecomposeComplexOps.cpp
+++ lib/Dialect/Torch/Transforms/DecomposeComplexOps.cpp
@@ -6135,13 +6135,11 @@ class DecomposeAtenNonzeroOp : public OpRewritePattern<AtenNonzeroOp> {
     int64_t inputRank = inputType.getSizes().size();
 
     // t_flat = t.flatten() # torch.flatten(t, 0, 0)
-    int64_t flattenedSize = 1;
-    if (inputType.hasSizes()) {
-      for (auto size : inputType.getSizes()) {
+    int64_t flattenedSize = Torch::kUnknownSize;
+    if (inputType.hasSizes() && inputType.areAllSizesKnown()) {
+      flattenedSize = 1;
+      for (auto size : inputType.getSizes())
         flattenedSize *= size;
-      }
-    } else {
-      flattenedSize = kUnknownSize;
     }
 
     auto flattendInputShape = SmallVector<int64_t>{flattenedSize};
@@ -12649,9 +12647,12 @@ public:
     if (inputRank > 1) {
       // If the input is not a 1-d tensor, we need to flatten it
       // to a 1D tensor before applying the strided indexing.
-      int64_t flattenedInputSize = 1;
-      for (int64_t size : inputSizes)
-        flattenedInputSize *= size;
+      int64_t flattenedInputSize = Torch::kUnknownSize;
+      if (llvm::all_of(inputSizes, [](int64_t s) { return s != Torch::kUnknownSize; })) {
+        flattenedInputSize = 1;
+        for (int64_t size : inputSizes)
+          flattenedInputSize *= size;
+      }
 
       auto flattenedInputTy =
           cast<BaseTensorType>(inputType.getWithSizesAndDtype(
@@ -12722,9 +12723,12 @@ public:
           loc, broadcastType, finalIndices, index, cstOne);
     }
 
-    int64_t flattenedResultSize = 1;
-    for (int64_t size : sizesInts)
-      flattenedResultSize *= size;
+    int64_t flattenedResultSize = Torch::kUnknownSize;
+    if (llvm::all_of(sizesInts, [](int64_t s) { return s != Torch::kUnknownSize; })) {
+      flattenedResultSize = 1;
+      for (int64_t size : sizesInts)
+        flattenedResultSize *= size;
+    }
 
     // Flattening the indices and adding the storage offset
     finalIndices = rewriter.create<AtenFlattenUsingIntsOp>(
